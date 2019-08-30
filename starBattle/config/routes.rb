48ef54597985c13f8b364e6c68a9a74311cec886
tiffanywilson <tiffanywilson387@gmail.com>
2019-08-30 15:38:59 -0500

    rails upgarde

--- starBattle/config/routes.rb
+++ starBattle/config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'battle_now/battle_now'
-
-  root 'home#index'
-  get '/about' => 'home#about'
-  get '/contact_us' => 'home#contact_us'
-  get '/how_it_works' => 'home#how_it_works'
-  get '/battle_now' => 'home#battle_now'
-
-  post '/feedback' => 'home#feedback'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
